18c54b03ac9e2fba1299bcab6fef964de9a9c025
Rich Tong <1782087+richtong@users.noreply.github.com>
2022-06-20 19:27:23 -0700

    feat: Makefile restarts containers works in Mac M1
    Makefile cuts the build container into pieces works with the modified
    _rich_cpu.sh

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-## Makefile for richtong/lib
-##
+## Makefile for ORB-SLAM3 docker build
+## -----------------------------------
 
 # Release tag
 TAG ?= 0.9
@@ -9,6 +9,9 @@ ORG ?= ghcr.io/netdrones
 IMAGE ?= orbslam3
 DOCKERFILE ?= Dockerfile_rich
 BUILD_CONTAINER ?= build_container_rich
+
+## all: download data, build container, run image and build artifacts
+all: download build container exec
 
 ## download: Download of the dataset
 # only if MH01.zip is not present and MH01 not unzipped
@@ -28,10 +31,29 @@ build:
 build_cuda:
 	ORG="$(ORG)" IMAGE="$(IMAGE)" DOCKERFILE="$(DOCKERFILE)" ./build_image.sh -t cuda
 
-## container: starts cpu build container to create artifacts in host
+## container: pull latest image, remove current running container, start a new one and build artifacts
 .PHONY: container
-container:
-	ORG="$(ORG)" IMAGE="$(IMAGE)" "./$(BUILD_CONTAINER)_cpu.sh"
+container: pull remove start compile
+
+## compile: compile ORB-SLAM3 with build image into host file system
+.PHONY: compile
+compile:
+	COMPILE=true ORG="$(ORG)" IMAGE="$(IMAGE)" "./$(BUILD_CONTAINER)_cpu.sh"
+
+## start: stop any running container, start a new one
+.PHONY: start
+start:
+	START=true ORG="$(ORG)" IMAGE="$(IMAGE)" "./$(BUILD_CONTAINER)_cpu.sh"
+
+## remove: remove container and artifacts
+.PHONY: remove
+remove:
+	REMOVE=true ORG="$(ORG)" IMAGE="$(IMAGE)" "./$(BUILD_CONTAINER)_cpu.sh"
+
+## pull: pull image
+.PHONY: pull
+pull:
+	PULL=true ORG="$(ORG)" IMAGE="$(IMAGE)" "./$(BUILD_CONTAINER)_cpu.sh"
 
 
 ## container_cuda: starts cuda build container to create artifacts in host
--- build_container_rich_cpu.sh
+++ build_container_rich_cpu.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
 # checking if you have nvidia gpu
+## this pulls the latest build container
+##
 #
 # Catch more errors like pipefail
 set -ueo pipefail && SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="${SCRIPT_DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"}"
-# do not build the container just exec it by default
-BUILD="${BUILD:-true}"
-# set remove the build artifacts
-REMOVE="${REMOVE:-true}"
-# set restart if you want to restart the container
-RESTART="${RESTART:-true}"
 
+# the four phases of building the artifacts with a running container
+# set to pull the latst build image
+PULL="${PULL:-false}"
+# set remove the build artifacts
+REMOVE="${REMOVE:-false}"
+# set restart if you want to restart the container
+START="${START:-false}"
+# do not build the container just exec it by default
+COMPILE="${COMPILE:-false}"
+
+PLATFORM="${PLATFORM:-linux/amd64}"
+TAG="${TAG:-ubuntu18_melodic_cpu}"
 ORG="${ORG:-jahaniam}"
 IMAGE="${IMAGE:-orbslam3}"
-echo "$SCRIPTNAME: Using docker org $ORG"
 
 if command -v nvidia-smi && nvidia-smi | grep -q Driver; then
 	echo "******************************"
@@ -46,7 +53,7 @@ if [[ $OSTYPE =~ darwin ]]; then
 	open -a XQuartz
 	xhost +localhost
 	# note single quotes to preserve the double quotes
-	X11_PARMS=(
+	DOCKER_FLAGS=(
 		-e DISPLAY="host.docker.internal:0"
 	)
 else
@@ -57,64 +64,55 @@ else
 	touch "$XAUTH"
 	xauth nlist "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 	xhost +local:docker
-	X11_PARMS=(
+	# do not quite understand why /etc/group needs to be matched
+	DOCKER_FLAGS=(
 		-e "DISPLAY=$DISPLAY"
 		-e "XAUTHORITY=$XAUTH"
 		-v "$XSOCK:$XSOCK:rw"
+		-v /etc/group:/etc/group:ro
 	)
 fi
 
-echo "$SCRIPTNAME: set environment variable BUILD=true if you want to remove the container"
-if $BUILD; then
-	echo "Building container"
-	docker pull "$ORG/$IMAGE:ubuntu18_melodic_cpu"
+# echo "$SCRIPTNAME: set environment variable PULL=true if you want to remove the container"
+if $PULL; then
+	echo "$SCRIPTNAME: Pulling container"
+	docker pull "$ORG/$IMAGE:$TAG"
 
 fi
 
-echo "$SCRIPTNAME: export REMOVE=true to remove build artifacts"
+#echo "$SCRIPTNAME: export REMOVE=true to remove build artifacts"
 if $REMOVE; then
 	echo "$SCRIPTNAME: Removing build artifacts"
 	if [[ -d ORB_SLAM3 ]]; then
 		rm -rf ORB_SLAM3
-		mkdir -p ORB_SLAM3
 	fi
 fi
 
-echo "$SCRIPTNAME: export RESTART=true if you want to restart the container"
-if $RESTART; then
-	echo "Restarting container"
+#echo "$SCRIPTNAME: export START=true if you want to restart the container"
+if $START; then
+	echo "$SCRIPTNAME: Remove running container"
 	docker rm -f "$IMAGE" &>/dev/null
+
+	# Create a new container
+	echo "$SCRIPTNAME: start a new ORB_SLAM3 container"
+	# make sure the directory exists
+	mkdir -p ORB_SLAM3
+	mkdir -p Datasets
+	# note we need --platform so this will run on M1 Macs
+	docker run -td --privileged --net=host --ipc=host \
+		--platform "$PLATFORM" \
+		--name="$IMAGE" \
+		"${DOCKER_FLAGS[@]}" \
+		-e "QT_X11_NO_MITSHM=1" \
+		-e ROS_IP=127.0.0.1 \
+		--cap-add=SYS_PTRACE \
+		-v "$(pwd)/Datasets:/Datasets" \
+		-v "$(pwd)/ORB_SLAM3:/ORB_SLAM3" \
+		"$ORG/$IMAGE:$TAG" bash
 fi
 
-# Create a new container
-echo "$SCRIPTNAME: start ORB_SLAM3 container"
-# shellcheck disable=SC2086
-echo $SCRIPTNAME: docker run -td --privileged --net=host --ipc=host \
-	--name="$IMAGE" \
-	--platform linux/amd64 \
-	"${X11_PARMS[@]}" \
-	-e "QT_X11_NO_MITSHM=1" \
-	-e ROS_IP=127.0.0.1 \
-	--cap-add=SYS_PTRACE \
-	-v /etc/group:/etc/group:ro \
-	-v "$(pwd)/Datasets:/Datasets" \
-	-v "$(pwd)/ORB_SLAM3:/ORB_SLAM3" \
-	"$ORG/$IMAGE:cpu" bash
-
-# note we need --platform so this will run on M1 Macs
-docker run -td --privileged --net=host --ipc=host \
-	--platform linux/amd64 \
-	--name="$IMAGE" \
-	"${X11_PARMS[@]}" \
-	-e "QT_X11_NO_MITSHM=1" \
-	-e ROS_IP=127.0.0.1 \
-	--cap-add=SYS_PTRACE \
-	-v /etc/group:/etc/group:ro \
-	-v "$(pwd)/Datasets:/Datasets" \
-	-v "$(pwd)/ORB_SLAM3:/ORB_SLAM3" \
-	"$ORG/$IMAGE:ubuntu18_melodic_cpu" bash
-
-if $BUILD; then
+if $COMPILE; then
+	echo "$SCRIPTNAME: Building artifacts in ORB_SLAM3"
 	# Git pull orbslam and compile
 	docker exec -it "$IMAGE" bash -i -c "git clone -b docker_opencv3.2_fix https://github.com/jahaniam/ORB_SLAM3 /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
 	# Compile ORBSLAM3-ROS
