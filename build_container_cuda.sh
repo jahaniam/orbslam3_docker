576f385c4eaeca486679fc0c0e75302db64ac6df
Ali Jahani <jahaniam@ualberta.ca>
2023-11-05 13:17:45 -0700

    Update build_container_cuda.sh
    Fix files not found

--- build_container_cuda.sh
+++ build_container_cuda.sh
@@ -44,7 +44,7 @@ docker run -td --privileged --net=host --ipc=host \
     jahaniam/orbslam3:ubuntu20_noetic_cuda bash
 
 # Git pull orbslam and compile
-docker exec -it orbslam3 bash -i -c "git clone https://github.com/UZ-SLAMLab/ORB_SLAM3 /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
+docker exec -it orbslam3 bash -i -c "git clone -b add_euroc_example.sh git@github.com:jahaniam/ORB_SLAM3.git /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
 # Compile ORBSLAM3-ROS
 docker exec -it orbslam3 bash -i -c "echo 'ROS_PACKAGE_PATH=/opt/ros/melodic/share:/ORB_SLAM3/Examples/ROS'>>~/.bashrc && source ~/.bashrc && cd /ORB_SLAM3 && chmod +x build_ros.sh && ./build_ros.sh"
 
